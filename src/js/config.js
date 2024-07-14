ddf5d0f4184709f7421d31232d78be236d5c427e
yuvath <13.bscs.147@gmail.com>
2024-07-14 14:30:57 +0500

    Add readme

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '9044ae92-6f30-412e-8c3f-a15646df3123';
 export const MODAL_CLOSE_SEC = 2.5;
